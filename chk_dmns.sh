3b0e12fbce453657fe710d52bf4be76b04055376
alttaby4 <alttaby4@gmail.com>
2026-04-13 09:55:54 +0500

    Create chk_dmns.sh

---
+++ chk_dmns.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Список доменов
+DOMAINS=(
+    "new.tv.team" "new.tvteam.pro" "new.tvteam.eu" "speed.tv.team" "speed.tvteam.pro"
+    "speed.tv.in" "speed.hls.ga" "webplayer.tv.in" "tv.team" "tvteam.eu"
+    "speed.tvteam.eu" "wiki.tv.in" "new.tvtm.one" "tvtm.one" "new.hls.ga"
+     "epg.team" "rus.epg.team" "tv.team" "team.ga"
+    "tvteam.eu" "tvteam.pro"
+)
+
+# Заголовки таблицы
+echo -e "\e[1;34m%-18s %-10s %-12s %-10s %-15s\e[0m" "DOMAIN" "PING" "LATENCY" "SSL DAYS" "STATUS"
+echo "--------------------------------------------------------------------------------"
+
+for DOMAIN in "${DOMAINS[@]}"; do
+    # 1. Проверка Пинга и получение времени ответа
+    PING_OUT=$(ping -c 1 -W 1 "$DOMAIN" 2>/dev/null)
+    
+    if [ $? -eq 0 ]; then
+        PING_RES="\e[1;32mUP\e[0m"
+        # Извлекаем время из строки "time=45.2 ms"
+        LATENCY=$(echo "$PING_OUT" | grep 'time=' | awk -F'time=' '{print $2}' | awk '{print $1 " ms"}')
+    else
+        PING_RES="\e[1;31mDOWN\e[0m"
+        LATENCY="---"
+    fi
+
+    # 2. Проверка SSL (порт 443)
+    END_DATE_STR=$(timeout 2 openssl s_client -servername "$DOMAIN" -connect "$DOMAIN":443 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null | cut -d= -f2)
+
+    if [ -z "$END_DATE_STR" ]; then
+        SSL_DAYS="N/A"
+        STATUS="\e[1;31mSSL ERROR\e[0m"
+    else
+        # Расчет дней до истечения
+        END_DATE_S=$(date -d "$END_DATE_STR" +%s)
+        CURRENT_DATE_S=$(date +%s)
+        DAYS_LEFT=$(( (END_DATE_S - CURRENT_DATE_S) / 86400 ))
+        SSL_DAYS="$DAYS_LEFT"
+
+        if [ "$DAYS_LEFT" -le 0 ]; then
+            STATUS="\e[1;31mEXPIRED\e[0m"
+        elif [ "$DAYS_LEFT" -le 4 ]; then
+            STATUS="\e[1;33mRENEW SOON\e[0m"
+        else
+            STATUS="\e[1;32mOK\e[0m"
+        fi
+    fi
+
+    # Вывод данных в таблицу
+    printf "%-18s %-20b %-12s %-10s %b\n" "$DOMAIN" "$PING_RES" "$LATENCY" "$SSL_DAYS" "$STATUS"
+done
