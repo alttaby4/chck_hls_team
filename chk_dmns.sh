cbf338ae2af8eff4d346eb17a6d9153e8c44e68b
alttaby4 <alttaby4@gmail.com>
2026-04-14 08:16:30 +0500

    Update chk_dmns.sh

--- chk_dmns.sh
+++ chk_dmns.sh
@@ -5,8 +5,8 @@ DOMAINS=(
     "new.tv.team" "new.tvteam.pro" "new.tvteam.eu" "speed.tv.team" "speed.tvteam.pro"
     "speed.tv.in" "speed.hls.ga" "webplayer.tv.in" "tv.team" "tvteam.eu"
     "speed.tvteam.eu" "wiki.tv.in" "rus.tvtm.one" "tvtm.one" "rus.hls.ga"
-     "epg.team" "rus.epg.team" "tv.team" "speed.team.ga"
-    "tvteam.eu" "tvteam.pro" 
+     "epg.team" "rus.epg.team" "tv.team" "speed.team.ga" "1.1usd.tv"
+    "2.1usd.tv" "tvteam.pro" "de.1usd.tv" "ua.1usd.tv" "troya.tv" "troya.info" "chat.tv.in"
 )
 
 # Заголовки таблицы
