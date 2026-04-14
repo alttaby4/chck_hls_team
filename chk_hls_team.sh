8db7e51679d9d9d778291518d5307b08d3898339
alttaby4 <alttaby4@gmail.com>
2026-04-14 09:51:47 +0500

    Update chk_hls_team.sh

--- chk_hls_team.sh
+++ chk_hls_team.sh
@@ -11,97 +11,16 @@ DOMAINS=(
 )
 
 # Заголовки таблицы
-echo -e "\e[1;34m%-18s %-10s %-12s %-10s %-12s %-20s %-15s\e[0m" "DOMAIN" "PING" "LATENCY" "SSL DAYS" "ASN" "PROVIDER" "STATUS"
-echo "-----------------------------------------------------------------------------------------------------------------------"
+echo -e "\e[1;34m%-18s %-10s %-12s %-10s %-15s\e[0m" "DOMAIN" "PING" "LATENCY" "SSL DAYS" "STATUS"
+echo "--------------------------------------------------------------------------------"
 
 for DOMAIN in "${DOMAINS[@]}"; do
-    # 0. Получение IP
-    IP=$(dig +short "$DOMAIN" | tail -n1)
-    
-    if [ -z "$IP" ]; then
-        ASN="---"
-        PROVIDER="DNS ERROR"
-    else
-        # Запрос к специализированному сервису Cymru (возвращает "ASN | IP | Name")
-        NET_DATA=$(whois -h whois.cymru.com " -v $IP" | tail -n1)
-        ASN=$(echo "$NET_DATA" | awk -F'|' '{print "AS"$1}' | xargs)
-        PROVIDER=$(echo "$NET_DATA" | awk -F'|' '{print $3}" | "$4' | cut -c1-20 | xargs)
-        
-        [ -z "$ASN" ] || [ "$ASN" == "ASAS" ] && ASN="---"
-        [ -z "$PROVIDER" ] && PROVIDER="Unknown"
-    fi
-
-    # 1. Проверка Пинга
-    PING_OUT=$(ping -c 1 -W 1 "$DOMAIN" 2>/dev/null)
-    if [ $? -eq 0 ]; then
-        PING_RES="\e[1;32mUP\e[0m"
-        LATENCY=$(echo "$PING_OUT" | grep 'time=' | awk -F'time=' '{print $2}' | awk '{print $1 " ms"}')
-    else
-        PING_RES="\e[1;31mDOWN\e[0m"
-        LATENCY="---"
-    fi
-
-    # 2. Проверка SSL
-    END_DATE_STR=$(timeout 3 openssl s_client -servername "$DOMAIN" -connect "$DOMAIN":443 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null | cut -d= -f2)
-
-    if [ -z "$END_DATE_STR" ]; then
-        SSL_DAYS="N/A"
-        STATUS="\e[1;31mSSL ERROR\e[0m"
-    else
-        END_DATE_S=$(date -d "$END_DATE_STR" +%s)
-        CURRENT_DATE_S=$(date +%s)
-        DAYS_LEFT=$(( (END_DATE_S - CURRENT_DATE_S) / 86400 ))
-        SSL_DAYS="$DAYS_LEFT"
-
-        if [ "$DAYS_LEFT" -le 0 ]; then
-            STATUS="\e[1;31mEXPIRED\e[0m"
-        elif [ "$DAYS_LEFT" -le 4 ]; then
-            STATUS="\e[1;33mRENEW SOON\e[0m"
-        else
-            STATUS="\e[1;32mOK\e[0m"
-        fi
-    fi
-
-    # Вывод
-    printf "%-18s %-20b %-12s %-10s %-12s %-20s %b\n" "$DOMAIN" "$PING_RES" "$LATENCY" "$SSL_DAYS" "$ASN" "$PROVIDER" "$STATUS"
-done#!/bin/bash
-
-# Список доменов
-DOMAINS=(
-    "1.hls.ga" "3.hls.ga" "4.hls.ga" "6.hls.ga" "2.hls.ga"
-    "5.hls.ga" "7.hls.ga" "11.hls.ga" "12.hls.ga" "10.hls.ga"
-    "9.hls.ga" "8.hls.ga" "13.hls.ga" "16.hls.ga" "hk.hls.ga"
-    "de.hls.ga" "15.hls.ga" "14.hls.ga" "ru.hls.ga" "ru2.hls.ga"
-    "ru3.hls.ga" "nl.hls.ga" "nl2.hls.ga" "ru7.hls.ga" "ua.hls.ga"
-    "ua2.hls.ga" "ru4.hls.ga" "ru5.hls.ga" "ru6.hls.ga" "17.hls.ga" "pl.hls.ga"
-)
-
-# Заголовки таблицы (обновлены под новые колонки)
-echo -e "\e[1;34m%-18s %-10s %-12s %-10s %-12s %-20s %-15s\e[0m" "DOMAIN" "PING" "LATENCY" "SSL DAYS" "ASN" "PROVIDER" "STATUS"
-echo "-----------------------------------------------------------------------------------------------------------------------"
-
-for DOMAIN in "${DOMAINS[@]}"; do
-    # 0. Получение IP и данных о сети
-    IP=$(dig +short "$DOMAIN" | tail -n1)
-    
-    if [ -z "$IP" ]; then
-        ASN="---"
-        PROVIDER="DNS ERROR"
-    else
-        # Запрос к whois (используем radb.net для скорости получения ASN и descr)
-        WHOIS_DATA=$(whois -h whois.radb.net "$IP" 2>/dev/null)
-        ASN=$(echo "$WHOIS_DATA" | grep -i "origin" | awk '{print $2}' | head -n1)
-        PROVIDER=$(echo "$WHOIS_DATA" | grep -i "descr" | head -n1 | sed 's/descr://g' | xargs | cut -c1-20)
-        
-        [ -z "$ASN" ] && ASN="Unknown"
-        [ -z "$PROVIDER" ] && PROVIDER="Unknown"
-    fi
-
     # 1. Проверка Пинга и получение времени ответа
     PING_OUT=$(ping -c 1 -W 1 "$DOMAIN" 2>/dev/null)
     
     if [ $? -eq 0 ]; then
         PING_RES="\e[1;32mUP\e[0m"
+        # Извлекаем время из строки "time=45.2 ms"
         LATENCY=$(echo "$PING_OUT" | grep 'time=' | awk -F'time=' '{print $2}' | awk '{print $1 " ms"}')
     else
         PING_RES="\e[1;31mDOWN\e[0m"
@@ -131,5 +50,5 @@ for DOMAIN in "${DOMAINS[@]}"; do
     fi
 
     # Вывод данных в таблицу
-    printf "%-18s %-20b %-12s %-10s %-12s %-20s %b\n" "$DOMAIN" "$PING_RES" "$LATENCY" "$SSL_DAYS" "$ASN" "$PROVIDER" "$STATUS"
+    printf "%-18s %-20b %-12s %-10s %b\n" "$DOMAIN" "$PING_RES" "$LATENCY" "$SSL_DAYS" "$STATUS"
 done
