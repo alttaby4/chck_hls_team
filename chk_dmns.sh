4dd8db52c592487b8728931e950737a6484623c9
alttaby4 <alttaby4@gmail.com>
2026-04-27 13:50:04 +0500

    Update chk_dmns.sh

--- chk_dmns.sh
+++ chk_dmns.sh
@@ -4,7 +4,7 @@
 DOMAINS=(
     "new.tv.team" "new.tvteam.pro" "new.tvteam.eu" "speed.tv.team" "speed.tvteam.pro"
     "speed.tv.in" "speed.hls.ga" "webplayer.tv.in" "tv.team" "tvteam.eu" "team.ga"
-    "speed.tvteam.eu" "wiki.tv.in" "rus.tvtm.one" "tvtm.one" "rus.hls.ga"
+    "speed.tvteam.eu" "wiki.tv.in" "rus.tvtm.one" "tvtm.one" "rus.hls.ga" "hls.ga"
      "epg.team" "rus.epg.team" "tv.team" "speed.team.ga" "1.1usd.tv"
     "2.1usd.tv" "tvteam.pro" "de.1usd.tv" "ua.1usd.tv" "troya.tv" "troya.info" "chat.tv.in"
     "speed.troya.tv" "speed.troya.info" "speed.tvteam.eu" "teamplay.city"
